69da797fc4b3b2f41e8de9613f11fe13ced54018
PushkrajDeshmukh5959 <pm.deshmukh2611@gmail.com>
2024-06-04 15:21:07 +0530

    github coyp

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,21 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
  
 export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+    publicRoutes: [
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+      ],
+      ignoredRoutes: [
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+      ]
 });
  
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
  
